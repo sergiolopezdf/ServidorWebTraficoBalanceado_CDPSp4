de65ccb6c0beef9dd333accab5cbe3c93f120084
Sergio López <sergio.lopezd@alumnos.upm.es>
2017-11-16 20:41:29 +0100

    Editing XML Ok

--- pfinalp1.py
+++ pfinalp1.py
@@ -10,18 +10,25 @@ from lxml import etree
 #Funcion que automatiza la creacion de ficheros .qcow2 y XML
 def crear():
    	# Se copia el archivo
-    subprocess.call("sudo cp plantilla-vm-p3.xml prueba1.xml", shell=True)
-    subprocess.call("sudo chmod 777 prueba1.xml", shell=True)
+    #subprocess.call("sudo cp plantilla-vm-p3.xml prueba1.xml", shell=True)
+    #subprocess.call("sudo chmod 777 prueba1.xml", shell=True)
    	# Falta copiar VM
 
     # Abre el archivo XML copiado
-    plantilla = etree.parse('prueba1.xml')
+    plantilla = etree.parse('plantilla-vm-p3.xml')
 
-    #Localiza la etiqueta a modificar
-    source = plantilla.find('devices/disk/source')#.attrib
+    #Localiza las etiqueta a modificar
+    sourceFile = plantilla.find('devices/disk/source')
+    sourceFile.set("file", "prueba1.qcow2")
 
-    print (source)
-    #source['file'] = "prueba1.qcow2"
+    name = plantilla.find('name')
+    name.text = "VM1"
+
+    sourceBridge = plantilla.find('devices/interface/source')
+    sourceBridge.set("bridge", "LAN1") # O LAN2
+
+    # Exportamos a un nuevo archivo 
+    plantilla.write(open('plantilla1.xml', 'w'), encoding='UTF-8')
 
 
 crear()
